b23328f7ea8a3b54f26f08a1cedc9a7b1d6cafbc
Elina Karapetian <elina.karapetyan.00@mail.ru>
2024-01-28 20:49:21 +0300

    Login-screen #comment create project

---
+++ Ideas-for-office-environment/Ideas-for-office-environment/Ideas_for_office_environmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ideas_for_office_environmentApp.swift
+//  Ideas-for-office-environment
+//
+//  Created by Elina Karapetian on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ideas_for_office_environmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
